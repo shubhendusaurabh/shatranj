acb3380f376680147fcdf58502c0f34817fdebf2
shubhu <shubhendu.saurabh@hotmail.com>
2015-12-03 00:50:42 +0530

    movign to angular

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+(function() {
+  'use strict';
+
+  angular.module('shatranj', ['ngMaterial', 'ngRoute', 'shatranj.factory']);
+
+}());
---
+++ public/js/matchCtrl.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var matchCtrl = angular.module('shatranj.controllers', []);
